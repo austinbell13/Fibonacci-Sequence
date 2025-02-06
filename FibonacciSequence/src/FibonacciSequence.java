78559931acf46cd5d19d28836ae69321bf98d2d1
Austin Bell <austin13bell@gmail.com>
2025-02-06 12:25:06 -0500

    Added fibFast (top-down dp) and fibFaster (bottom-up dp).

--- FibonacciSequence/src/FibonacciSequence.java
+++ FibonacciSequence/src/FibonacciSequence.java
@@ -1,9 +1,5 @@
 public class FibonacciSequence {
 
-    public static void main(String[] args) {
-        runFib();
-    }
-
     static int fCount = 0;
     static int timeStep = 0;
 
@@ -33,10 +29,79 @@ public class FibonacciSequence {
         return fibN;
     }
 
+    private static long fibNoViz(int n) {
+        if (n == 0) {
+            return 0;
+        } if (n == 1) {
+            return 1;
+        }
+        long fibPrev = fibNoViz(n - 1);
+        long fibPrevPrev = fibNoViz(n - 2);
+        long fibN = fibPrev + fibPrevPrev;
+
+        return fibN;
+    }
+
     private static void runFib() {
         int n = 5;
         long fibN = fib(n);
         System.out.println("fib(" + n + "): " + fibN);
+    }
+
+    private static void runFibMany() {
+        for (int i = 0; i <= 50; i++) {
+            long startTime = System.nanoTime();
+            long val = fib(i);
+            long endTime = System.nanoTime();
+            System.out.printf("fib(%d) = %d \t time: %fs\n", i, val, (endTime - startTime) / 1e9);
+        }
+    }
+
+    private static long fibFast(int n, Long[] mem) {
+        // Top-Down Dynamic programming (memorization).
+        if (mem[n] != null) {
+            return mem[n];
+        }
+        // First base case.
+        if (n == 0) {
+            return 0;
+        }
+        // Second base case.
+        if (n == 1) {
+            return 1;
+        }
+        long fibPrev = fibNoViz(n - 1);
+        long fibPrevPrev = fibNoViz(n - 2);
+        long fibN = fibPrev + fibPrevPrev;
+
+        mem[n] = fibN;
+
+        return mem[n];
+    }
+
+    public static void runFastFibMany() {
+        for (int i = 0; i <= 50; i++) {
+            long startTime = System.nanoTime();
+            // long val = fibFast(i, new Long[i + 1]);
+            long val = fibFaster(i);
+            long endTime = System.nanoTime();
+            System.out.printf("fibFast(%d) = %d \t time: %fs\n", i, val, (endTime - startTime) / 1e9);
+        }
+    }
+
+    // Bottom-up dynamic programming (Table-filling)
+    public static long fibFaster (int n) {
+        long[] dp = new long[Math.max(2, n + 1)];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        runFastFibMany();
     }
 
 }
