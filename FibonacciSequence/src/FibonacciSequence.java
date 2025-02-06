59b4f756bb901b599edd26e591d91ef6bb05fa5c
Austin Bell <austin13bell@gmail.com>
2025-02-06 11:52:04 -0500

    Added time stamps.

--- FibonacciSequence/src/FibonacciSequence.java
+++ FibonacciSequence/src/FibonacciSequence.java
@@ -3,23 +3,32 @@ public class FibonacciSequence {
     public static void main(String[] args) {
         runFib();
     }
+
     static int fCount = 0;
+    static int timeStep = 0;
+
     private static long fib(int n) {
-        ++fCount;
-        System.out.println("     |".repeat(fCount) + "st:fib(" + n + "): ?");
+        ++fCount; // Stack frame count.
+        // Stack frame visualization.
+        System.out.printf("t%03d |", ++timeStep); // Including a time step.
+        System.out.println("_____|".repeat(fCount) + "st:fib(" + n + "): ?");
         if (n == 0) {
-            System.out.println("     |".repeat(fCount) + "ed:fib(" + n + "): 0");
+            System.out.printf("t%03d |", ++timeStep); // Including a time step.
+            System.out.println("_____|".repeat(fCount) + "ed:fib(" + n + "): 0");
             --fCount;
             return 0;
         } if (n == 1) {
-            System.out.println("     |".repeat(fCount) + "ed:fib(" + n + "): 1");
+            System.out.printf("t%03d |", ++timeStep); // Including a time step.
+            System.out.println("_____|".repeat(fCount) + "ed:fib(" + n + "): 1");
             --fCount;
             return 1;
         }
         long fibPrev = fib(n - 1);
         long fibPrevPrev = fib(n - 2);
         long fibN = fibPrev + fibPrevPrev;
-        System.out.println("     |".repeat(fCount) + "ed:fib(" + n + "): " + fibN);
+
+        System.out.printf("t%03d |", ++timeStep); // Including a time step.
+        System.out.println("_____|".repeat(fCount) + "ed:fib(" + n + "): " + fibN);
         --fCount;
         return fibN;
     }
